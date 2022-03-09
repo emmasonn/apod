4c1427ecfa3c4b0437f8e9998f5b916fddba4619
emmasonn <words.cs1@gmail.com>
2022-03-09 13:19:44 +0100

    new commit

---
+++ lib/ui/recent_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RecentScreen extends StatefulWidget {
+  const RecentScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RecentScreen> createState() => _RecentScreenState();
+}
+
+class _RecentScreenState extends State<RecentScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+    );
+  }
+}
