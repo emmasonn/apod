ebb2958a899cef1722a3312c3c10b91bd42eae22
emmasonn <words.cs1@gmail.com>
2022-03-14 07:56:20 +0100

    create appStatemanager to replace tabmanager using provider pattern

---
+++ lib/provider_model/app_state_manager.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class ApodTab {
+  static const int today = 0;
+  static const int recent = 1;
+  static const int favorites = 2;
+  static const int journal = 3;
+}
+
+class AppStateManager extends ChangeNotifier {
+  bool _initialized = false;
+  bool _loggedIn = false;
+  int _selectedTab = ApodTab.today;
+
+  bool get isInitialized => _initialized;
+  bool get isLoggedIn => _loggedIn;
+  int get selectedTab => _selectedTab;
+
+  void logIn() {
+    _loggedIn = true;
+    notifyListeners();
+  }
+
+  void initialized() {
+    _initialized = true;
+    notifyListeners();
+  }
+
+  void goToPage(int index) {
+    _selectedTab = index;
+    notifyListeners();
+  }
+
+  void goToRecent() {
+    _selectedTab = ApodTab.recent;
+    notifyListeners();
+  }
+}
+
+final appStateManager = AppStateManager();
