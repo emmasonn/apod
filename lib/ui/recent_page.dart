75411ad023c38c0a94b9b55503b4c101444a88b3
emmasonn <words.cs1@gmail.com>
2022-03-20 09:07:04 +0100

    fixed recent page

--- lib/ui/recent_page.dart
+++ lib/ui/recent_page.dart
@@ -1,4 +1,9 @@
+import 'package:apod/api/mock_service.dart';
+import 'package:apod/cards/favorite_card.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../api_model/apod.dart';
 
 class RecentPage extends StatefulWidget {
   const RecentPage({Key? key}) : super(key: key);
@@ -10,8 +15,33 @@ class RecentPage extends StatefulWidget {
 class _RecentPageState extends State<RecentPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black,
-    );
+    final mockService = MockApodService();
+    return FutureBuilder(
+        future: mockService.getApods(),
+        builder: (BuildContext context, AsyncSnapshot<List<Apod>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            final apods = snapshot.data ?? [];
+            return ListView.builder(
+                itemCount: snapshot.data?.length ?? 0,
+                itemBuilder: (BuildContext context, int index) {
+                  return GestureDetector(
+                    onTap: () => context.go('/apod/${apods[index].id}'),
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                        top: index == 0 ? 12.0 : 0,
+                        bottom: 12.0,
+                      ),
+                      child: FavoriteCard(
+                        apod: apods[index],
+                      ),
+                    ),
+                  );
+                });
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        });
   }
 }
